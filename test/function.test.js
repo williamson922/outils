b91090cb5d23449ade20ea2536629368b81e1d76
Shenggg2000 <gohyoushengabc@gmail.com>
2022-09-06 13:16:09 +0800

    add array highest occurrence function and fix some test code bug

--- test/function.test.js
+++ test/function.test.js
@@ -77,27 +77,26 @@ describe('Function API:', function () {
 
   describe('#isPrimeNumber', function () {
     it(`outils.isPrimeNumber(1, function(){return false}) should return false`, function () {
-      assert(outils.isPrimeNumber(1));
+      assert(!outils.isPrimeNumber(1));
     });
     it(`outils.CheckEvenNumber(2, function(){return true}) should return true`, function () {
-      assert(!outils.isPrimeNumber(2));
+      assert(outils.isPrimeNumber(2));
     });
     it(`outils.CheckEvenNumber(35, function(){return false}) should return false`, function () {
-      assert(outils.isPrimeNumber(35));
+      assert(!outils.isPrimeNumber(35));
     });
   });
 
-    describe('#CheckEvenNumber', function(){
-
-        it(`outils.CheckEvenNumber(200, function(){return true}) should return true`, function(){
-           assert(outils.CheckEvenNumber(200))
-        })
-        it(`outils.CheckEvenNumber(195, function(){return false}) should return false`, function(){
-            assert(!outils.CheckEvenNumber(195))
-         })
-         it(`outils.CheckEvenNumber(186, function(){return true}) should return true`, function(){
-            assert(outils.CheckEvenNumber(186))
-         })
+  describe('#CheckEvenNumber', function () {
+    it(`outils.CheckEvenNumber(200, function(){return true}) should return true`, function () {
+      assert(outils.CheckEvenNumber(200))
     })
+    it(`outils.CheckEvenNumber(195, function(){return false}) should return false`, function () {
+      assert(!outils.CheckEvenNumber(195))
+    })
+    it(`outils.CheckEvenNumber(186, function(){return true}) should return true`, function () {
+      assert(outils.CheckEvenNumber(186))
+    })
+  })
 });
 
