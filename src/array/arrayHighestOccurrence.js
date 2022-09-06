123047c0393f9115f29879fb5f18960563f29e5d
Shenggg2000 <gohyoushengabc@gmail.com>
2022-09-06 13:09:03 +0800

    add array highest occurrence function

---
+++ src/array/arrayHighestOccurrence.js
@@ -0,0 +1,27 @@
+/**
+ * 
+ * @desc Find Highest Occurrence Element in Array
+ * @param {Array} array 
+ * @return {String|null}
+ */
+function arrayHighestOccurrence(array)
+{
+    if(array.length == 0)
+        return null;
+    var modeMap = {};
+    var maxEl = array[0], maxCount = 1;
+    for(var i = 0; i < array.length; i++)
+    {
+        var el = array[i];
+        if(modeMap[el] == null)
+            modeMap[el] = 1;
+        else
+            modeMap[el]++;  
+        if(modeMap[el] > maxCount)
+        {
+            maxEl = el;
+            maxCount = modeMap[el];
+        }
+    }
+    return maxEl;
+}
--- test/array.test.js
+++ test/array.test.js
@@ -1,17 +1,26 @@
 describe('Array API:', function () {
-    describe('#arrayEqual()', function () {
-        it(`outils.arrayEqual([0, 2, 3, 4], [1, 2, 3]) should return false`, function () {
-            assert.notEqual(outils.arrayEqual([0, 2, 3, 4], [1, 2, 3]), true)
-        });
-        it(`outils.arrayEqual([0, 2, 3], [1, 2, 3]) should return false`, function () {
-            assert.notEqual(outils.arrayEqual([0, 2, 3], [1, 2, 3]), true)
-        });
-        it('outils.arrayEqual([1, 2, 3], [1, 2, 3]) should return true', function () {
-            assert(outils.arrayEqual([1, 2, 3], [1, 2, 3]))
-        });
-        const arr = [8, 2, 3, 4, 7, 8]
-        it(`outils.arrayEqual([${arr},${arr}]) should return true`, function () {
-            assert(outils.arrayEqual(arr, arr))
-        });
+  describe('#arrayEqual()', function () {
+    it(`outils.arrayEqual([0, 2, 3, 4], [1, 2, 3]) should return false`, function () {
+      assert.notEqual(outils.arrayEqual([0, 2, 3, 4], [1, 2, 3]), true)
     });
+    it(`outils.arrayEqual([0, 2, 3], [1, 2, 3]) should return false`, function () {
+      assert.notEqual(outils.arrayEqual([0, 2, 3], [1, 2, 3]), true)
+    });
+    it('outils.arrayEqual([1, 2, 3], [1, 2, 3]) should return true', function () {
+      assert(outils.arrayEqual([1, 2, 3], [1, 2, 3]))
+    });
+    const arr = [8, 2, 3, 4, 7, 8]
+    it(`outils.arrayEqual([${arr},${arr}]) should return true`, function () {
+      assert(outils.arrayEqual(arr, arr))
+    });
+  });
+
+  describe('#arrayHighestOccurrence()', function () {
+    it(`outils.arrayHighestOccurrence(['A', 'B', 'C', 'A']) should return 'A'`, function () {
+      assert(outils.arrayHighestOccurrence(['A', 'B', 'C', 'A']) === "A")
+    });
+    it(`outils.arrayHighestOccurrence([]) should return null`, function () {
+      assert(outils.arrayHighestOccurrence([]) === null)
+    });
+  });
 });
